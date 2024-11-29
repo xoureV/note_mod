453e7eb8a330fc61971411deb1f4b2408d813445
Fin_Jaksch <20220263@htlstp.at>
2024-11-29 17:44:58 +0100

    startfiles

---
+++ src/main/java/net/first/note/NoteClient.java
@@ -0,0 +1,11 @@
+package net.first.note;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoteClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
